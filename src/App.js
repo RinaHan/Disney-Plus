4b03956ffb9f54974bc6f9419bfa17bb8d6d6175
RinaHan <rinahan4146@gmail.com>
2024-02-01 13:57:31 -0800

    call movie information using Axios

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-import styled from 'styled-components';
-import './App.css';
-import Nav from './components/Nav';
+import styled from "styled-components";
+import "./App.css";
+import Nav from "./components/Nav";
+import Banner from "./components/Banner";
 
 function App() {
   return (
-    <Container className="App">
-      <Nav/>
+    <Container className='App'>
+      <Nav />
+      <Banner />
     </Container>
   );
 }
@@ -13,19 +15,19 @@ function App() {
 export default App;
 
 const Container = styled.main`
- position: relative;
- min-height: calc(100vh - 250px);
- overflow-x:hidden;
- display:block;
- top:72px;
- padding:0 calc(3.5vw + 5px);
+  position: relative;
+  min-height: calc(100vh - 250px);
+  overflow-x: hidden;
+  display: block;
+  top: 72px;
+  padding: 0 calc(3.5vw + 5px);
 
- &:after{
-  background:url("/images/home-background.png")center center /cover no-repeat fixed;
-  content: '';
-  position:absolute;
-  inset: 0px;
-  opacity:1;
-  z-index:-1;
- }
-`
+  &:after {
+    background: url("/images/home-background.png") center center / cover no-repeat fixed;
+    content: "";
+    position: absolute;
+    inset: 0px;
+    opacity: 1;
+    z-index: -1;
+  }
+`;
--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,9 +1,53 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import axios from "../api/axios";
+import requests from "../api/request";
 
 function Banner() {
+  const [movie, setMovie] = useState([]);
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+  const fetchData = async () => {
+    //call now playing movies
+    const response = await axios.get(requests.fetchNowPlaying);
+    // console.log("re", response.data.results.length);
+
+    //call a movie id
+    const movieId =
+      response.data.results[Math.floor(Math.random() * response.data.results.length)].id;
+    // console.log('mo', movieId)
+
+    //call the movie's detail using the movie's id
+    const { data: movieDetail } = await axios.get(`movie/${movieId}`, {
+      params: { append_to_response: "video" },
+    });
+    // console.log('moId', movieDetail)
+
+    setMovie(movieDetail);
+    console.log("movieD", movie?.videos?.results[0]?.key);
+  };
+
   return (
-    <div>Banner</div>
-  )
+    <header
+      className='banner'
+      style={{
+        backgroundImage: `url('https://image.tmdb.org/t/p/original/${movie.backdrop_path})`,
+        backgroundPosition: "top center",
+        backgroundSize: "cover",
+        height: "300px",
+      }}
+    >
+      <div className='banner_contents'>
+        <hi className='banner_title'>{movie.title || movie.name || movie.original_name}</hi>
+        <div className='banner_buttons'>
+          {movie?.videos?.results[0]?.key && <button className='banner_button play'>Play</button>}
+        </div>
+        <h1 className='banner_desc'>{movie.overview}</h1>
+      </div>
+      <div className='banner--fadeBottom' />
+    </header>
+  );
 }
 
-export default Banner
+export default Banner;
