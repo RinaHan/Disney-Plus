6670acdfd700d7d82320095aff03e566f3cb3bb1
RinaHan <rinahan4146@gmail.com>
2024-02-04 01:39:03 -0800

    swiper styling

--- src/components/Row.js
+++ src/components/Row.js
@@ -49,6 +49,24 @@ const Row = (props, id) => {
         loop={true} //use loop ability
         navigation //arrow button
         pagination={{ clickable: true }} //page button
+        breakpoints={{
+          1378: {
+            slidesPerView: 6,
+            slidesPerGroup: 6,
+          },
+          998: {
+            slidesPerView: 5,
+            slidesPerGroup: 5,
+          },
+          625: {
+            slidesPerView: 4,
+            slidesPerGroup: 4,
+          },
+          0: {
+            slidesPerView: 3,
+            slidesPerGroup: 3,
+          },
+        }}
       >
         <Content id={id}>
           {movies.map((movie) => (
@@ -57,7 +75,7 @@ const Row = (props, id) => {
               <Wrap>
                 <img
                   key={movie.id}
-                  className='row_poster'
+                  // className='row_poster'
                   src={`https://image.tmdb.org/t/p/original/${movie.backdrop_path}`}
                   alt={movie.name}
                   onClick={() => handleClick(movie)}
@@ -79,4 +97,34 @@ const Container = styled.div`
 `;
 
 const Content = styled.div``;
-const Wrap = styled.div``
+const Wrap = styled.div`
+width: 95%;
+height: 95%;
+padding-top: 56.25%;
+border-radius: 10px;
+box-shadow: rgb(0 0 0 0/69%) 0px 26px 30px -10px,
+rgb(0 0 0 0/73% 0px 16px 10px -10px);
+cursor: pointer;
+overflow:hidden;
+position:relative;
+transition: all 250ms cubic-bezier(0.25, 0.46, 0.45, 0.94) 0s;
+border: 3px solid #484747eb;
+
+img{
+  inset: 0px;
+  display:block;
+  height:100%;
+  object-fit:cover:
+  opacity: 1;
+  position: absolute;
+  width:100%;
+  transition: opacity 500ms ease-in-out;
+  z-index:1;
+}
+&:hover{
+  box-shadow: rgb(0 0 0 / 80%) 0px 40px 58px -16px,
+  rgb(0 0 0 / 72%) 0px 30px 22px -40px;
+  transform: scale(0.98);
+  border-color: rgba(249, 249, 249, 0.8);
+}
+`;
